a04a3c5dfdd0cbb5ce2562b1c017995e2aeef79a
peytonkemp <peyton.kemp@gmail.com>
2021-03-04 11:42:37 -0600

    added nav bar

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,12 @@
+import { Route } from "react-router-dom";
+// import { Home } from "./Home"
+import { LocationProvider } from "./locations/LocationProvider"
+import { LocationList } from "./locations/LocationList"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+
+        </>
+    )
+}
